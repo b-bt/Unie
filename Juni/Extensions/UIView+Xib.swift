a70d852e00f712686c37e136954c25280eefbac3
Bruno Barbosa <bbt@cin.ufpe.br>
2019-07-14 05:31:51 -0300

    Added extension to load UIView from xib

---
+++ Juni/Extensions/UIView+Xib.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Xib.swift
+//  Juni
+//
+//  Created by Bruno Barbosa on 14/07/19.
+//  Copyright © 2019 Bruno Barbosa. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    func loadViewFromNib() -> UIView! {
+        let nibName: String = String(describing: type(of: self))
+        
+        let bundle: Bundle = Bundle(for: type(of: self))
+        let nib: UINib = UINib(nibName: nibName, bundle: bundle)
+        let view: UIView = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        return view
+    }
+}
