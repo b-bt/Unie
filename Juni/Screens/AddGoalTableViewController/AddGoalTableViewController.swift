41da79379cae40a2497fbbcc313046b9d1c75cc1
Bruno Barbosa <bbt@cin.ufpe.br>
2019-07-14 12:06:57 -0300

    Getting image from camera album

--- Juni/Screens/AddGoalTableViewController/AddGoalTableViewController.swift
+++ Juni/Screens/AddGoalTableViewController/AddGoalTableViewController.swift
@@ -11,22 +11,43 @@ import UIKit
 class AddGoalTableViewController: UITableViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var goalName: UITextField!
+    @IBOutlet weak var goalValue: UITextField!
+    @IBOutlet weak var goalLimitDate: UIDatePicker!
+    
+    let imagePicker = UIImagePickerController()
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.prefersLargeTitles = false
+        self.imagePicker.delegate = self
         
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectPicture))
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(loadPictureBtnTapped))
         self.imageView.addGestureRecognizer(tapRecognizer)
     }
 
+    @objc func loadPictureBtnTapped() {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func addGoal(_ sender: Any) {
-        // TODO
+        
     }
-    
-    @objc func selectPicture() {
-        print("oi")
-    }
-    
+}
 
+extension AddGoalTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = pickedImage
+            self.selectedImage = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
